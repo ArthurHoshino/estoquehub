232770ff6c2680615cd1ff558c0d85a84f92bc7a
Arthur <arthur.hoshino@gmail.com>
2025-05-10 17:34:37 -0300

    Arthur-1 - Adicionado token para sessão de usuário

--- db/migrations/002.CreateUsuarioTable.php
+++ db/migrations/002.CreateUsuarioTable.php
@@ -8,8 +8,9 @@ class CreateUsuarioTable {
                 CDUSNOME VARCHAR(100) NOT NULL,
                 CDUSEMAIL VARCHAR(100) NOT NULL,
                 CDUSCEL VARCHAR(20),
-                CDUSSENHA VARCHAR(45) NOT NULL,
+                CDUSSENHA VARCHAR(60) NOT NULL,
                 CDUSCARGOID INT UNSIGNED,
+                CDUSTOKEN VARCHAR(60),
 
                 PRIMARY KEY (CDUSID),
                 FOREIGN KEY (CDUSCARGOID) REFERENCES CDCARGO(CDCARID)
